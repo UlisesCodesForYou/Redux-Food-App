ff6223d905bde0f3f3f100be6fdaadc1d41e9308
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-03-06 21:39:51 -0800

    Made components more dynamic. Need to fix bug

--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-Slice";
+import cartSlice from "./cart-Slice";
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer },
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 
-
-export default store // This goes where the whole app is rendered. 
+export default store; // This goes where the whole app is rendered.
