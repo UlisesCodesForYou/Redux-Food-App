9587b6629b18a1cb7672571f2fc5b58a8d1b5017
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-03-11 09:46:35 -0800

    Added useEffect to be able to PUT requests.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,7 +7,14 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible); // Remember that this receives the redux state.  I just have to drill into it. 9
+  const cart = useSelector((state) => state.cart);
 
+  useEffect(() => {
+    fetch("https://menu-5b54e-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT", // This method replaces the previous data with new data.  
+      body: JSON.stringify(cart),  // This transforms the cart redux data to json data. 
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
